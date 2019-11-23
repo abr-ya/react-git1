29e0512f3003805951e926df477016c4731c10b4
abr-ya <gamer05@list.ru>
2019-11-23 12:28:06 +0300

    Реализован основной функционал.

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,7 @@
-import React, {useContext, useEffect} from 'react';
+import React, {Fragment, useContext, useEffect} from 'react';
 import {GithubContext} from '../context/github/githubContext';
 import {Link} from 'react-router-dom';
+import {Repos} from '../components/Repos/Repos';
 
 export const Profile = ({match}) => {
     const {getUser, getRepos, loading, user, repos} = useContext(GithubContext);
@@ -21,26 +22,68 @@ export const Profile = ({match}) => {
 
     //"развернём" юзера
     const {name, company, avatar_url, location, bio, blog,
-        login, html_url, followers,
+        login, html_url, followers, following,
         public_repos, public_gists
     } = user;
 
     return (
-        <div>
-            <h1>Profile page</h1>
-            <Link to="/" className="btn btn-link">На главную</Link>
+        <Fragment>
+            <h1>{name} profile page</h1>
+            <Link to="/" className="btn btn-link">вернуться к результатам поиска</Link>
 
             <div className="card mb-4">
                 <div className="card-body">
                     <div className="row">
-                        <div className="col-sm-3 text-center">
-                            <img src="" alt=""/>
-                            <h1>Name</h1>
-
+                        <div className="col-sm-4">
+                            <img src={avatar_url} alt={name} />
+                            {location && <p>Место: {location}</p>}
                         </div>
+                        <div className="col-sm-8">
+                            {
+                                bio && <Fragment>
+                                    <h3>BIO</h3>
+                                    <p>{bio}</p>
+                                </Fragment>
+                            }
+                            <a href={html_url}
+                                className="btn btn-dark"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                Открыть профиль
+                            </a>
+
+                            <ul>
+                                {login && <li>
+                                    <strong>Username: </strong> {login}
+                                </li>}
+                                {company && <li>
+                                    <strong>Company: </strong> {company}
+                                </li>}
+                                {blog && <li>
+                                    <strong>Blog: </strong> {blog}
+                                </li>}
+                            </ul>
+
+                            <div className="badge badge-primary">
+                                Подписчики: {followers}
+                            </div>
+                            <div className="badge badge-success">
+                                Подписан(а): {following}
+                            </div>
+                            <div className="badge badge-info">
+                                Репозитории: {public_repos}
+                            </div>
+                            <div className="badge badge-dark">
+                                Gists: {public_gists}
+                            </div>
+                        </div>{// col-sm-8
+                        }
                     </div>
                 </div>
             </div>
-        </div>
+
+            <Repos repos={repos} />
+        </Fragment>
     )
 }

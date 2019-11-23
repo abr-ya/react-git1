106ec6b89fd328ea6c4d3467c598abe7f965002a
Yaroslav <gamer05@list.ru>
2019-11-23 11:12:33 +0300

    Подготовлены данные для страницы пользователя.

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -4,24 +4,43 @@ import {Link} from 'react-router-dom';
 
 export const Profile = ({match}) => {
     const {getUser, getRepos, loading, user, repos} = useContext(GithubContext);
-    const name = match.params.name;
+    const urlName = match.params.name;
 
     // после отрисовки
     // эффект и список зависимостей, пустой массив - выполнится один раз
     useEffect(() => {
-        getUser(name);
-        getRepos(name);        
+        getUser(urlName);
+        getRepos(urlName);        
     }, []);
 
     if (loading) {
         return <p className="text-center">Загрузка...</p>
     }
 
-    console.log(match)
+    //console.log(match)
+
+    //"развернём" юзера
+    const {name, company, avatar_url, location, bio, blog,
+        login, html_url, followers,
+        public_repos, public_gists
+    } = user;
+
     return (
         <div>
             <h1>Profile page</h1>
             <Link to="/" className="btn btn-link">На главную</Link>
+
+            <div className="card mb-4">
+                <div className="card-body">
+                    <div className="row">
+                        <div className="col-sm-3 text-center">
+                            <img src="" alt=""/>
+                            <h1>Name</h1>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
